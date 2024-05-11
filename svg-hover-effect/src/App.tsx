631777900e12fee01bdc63d164ae1a5aed2acc06
Khushwant <khush123098@gmail.com>
2024-05-12 00:27:52 +0530

    first commit

--- svg-hover-effect/src/App.tsx
+++ svg-hover-effect/src/App.tsx
@@ -1,34 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { CheckIcon } from "@heroicons/react/24/solid"
+import { CircleStackIcon } from "@heroicons/react/24/solid"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className="w-full h-screen flex place-items-center justify-center">
+      <section className="card">
+        <div className="flex flex-col w-2/5 justify-between">
+          <div className="flex flex-col gap-5" >
+            <CircleStackIcon className="w-14 rounded-lg bg-neutral-950/70 stroke-emerald-500 p-2 shadow-inner" />
+            <h1 className="font-poppins text-neutral-200 tracking-wide text-2xl" >
+              Databse
+            </h1>
+            <p className="-mt-2 font-poppins text-neutral-500 tracing-wide" >
+              Every project is a full Postgres database, the world's most trusted relational database.
+            </p>
+          </div>
+          <div className="flex flex-column text-neutral-200 tracking-wide" >
+            <span className="flex flex-row gap-2" >
+              <CheckIcon className="w-5" />
+              <p>100% portable</p>
+            </span>
+            <span className="flex flex-row gap-2" >
+              <CheckIcon className="w-5" />
+              <p>Built-in Auth with RLS</p>
+            </span>
+            <span className="flex flex-row gap-2" >
+              <CheckIcon className="w-5" />
+              <p>Easy to extend</p>
+            </span>
+          </div>
+        </div>
+      </section>
+    </main>
   )
 }
 
